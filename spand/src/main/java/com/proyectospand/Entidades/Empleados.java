8b0a55dbb0b8786994db67a2641f532c87f2888f
Angel Lomeli <2022030087@upsin.edu.mx>
2024-07-29 22:44:26 -0700

    Arreglando el problema del merge

--- spand/src/main/java/com/proyectospand/Entidades/Empleados.java
+++ spand/src/main/java/com/proyectospand/Entidades/Empleados.java
@@ -5,9 +5,4 @@ public class Empleados {
     private String nombre;
     private String apellido;
     private String calle;
-<<<<<<< HEAD:spand/src/main/java/com/proyectospand/Entidades/Empleado.java
-    private String nss;
-=======
-    private String colonia;
->>>>>>> 15ec1f8acbb5b8ce46c8d53b3a08f082f6c1eaf0:spand/src/main/java/com/proyectospand/Entidades/Empleados.java
 }
