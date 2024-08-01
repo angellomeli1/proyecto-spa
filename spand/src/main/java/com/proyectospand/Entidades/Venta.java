2cf43231c554618034c061f22896612fc62e38e6
Omar Rendon <2022030072@upsin.edu.mx>
2024-07-31 21:06:17 -0700

    Terminé la clase objeto "Venta". Quedan pendientes sus métodos

--- spand/src/main/java/com/proyectospand/Entidades/Venta.java
+++ spand/src/main/java/com/proyectospand/Entidades/Venta.java
@@ -4,7 +4,7 @@ public class Venta {
     // Atributos
     private int idVenta;
     private Clientes idCliente;
-    private Empleados iEmpleados;
+    private Empleados idEmpleados;
     private String fechaVenta;
     private double total;
 
@@ -12,5 +12,71 @@ public class Venta {
     // Constructor vacío
     public Venta(){
         this.idVenta = 0;
+        this.idCliente = null;
+        this.idEmpleados = null;
+        this.fechaVenta = "";
+        this.total = 0.0;
+    }// Fin del constructor vacío
+
+    // Constructor con parámetros
+    public Venta(int idVenta, Clientes idCliente, Empleados idEmpleados, String fechaVenta, double total) {
+        this.idVenta = idVenta;
+        this.idCliente = idCliente;
+        this.idEmpleados = idEmpleados;
+        this.fechaVenta = fechaVenta;
+        this.total = total;
+    }// Fin del constructor con parámetros
+
+    // Constructor con copia
+    public Venta(Venta venta){
+        this.idVenta = venta.idVenta;
+        this.idCliente = venta.idCliente;
+        this.idEmpleados = venta.idEmpleados;
+        this.fechaVenta = venta.fechaVenta;
+        this.total = venta.total;
+    }// Fin del constructor con copia
+
+    // ENCAPSULAMIENTO
+    public int getIdVenta() {
+        return idVenta;
     }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public Clientes getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Clientes idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Empleados getIdEmpleados() {
+        return idEmpleados;
+    }
+
+    public void setIdEmpleados(Empleados idEmpleados) {
+        this.idEmpleados = idEmpleados;
+    }
+
+    public String getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(String fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    // Métodos
+
 }
