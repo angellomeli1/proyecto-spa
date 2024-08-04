c046aed293b521bb8459a34b7efd200ea57492d4
Angel Lomeli <2022030087@upsin.edu.mx>
2024-08-04 14:15:28 -0700

    Se realizaron unos cambios al tipo de conexion de la base de datos, ahora aplicando el pool de conexiones para tener acceso a la base de datos, todo esto con la libreria apache commons-dbcp2

--- spand/src/main/java/com/proyectospand/ConexionDB/conexionDB.java
+++ spand/src/main/java/com/proyectospand/ConexionDB/conexionDB.java
@@ -3,21 +3,31 @@ package com.proyectospand.ConexionDB;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
 public class conexionDB {
 
+    //Partes esenciales para la conexion a la base de datos mediante el pool de conexiones
     private final String DB = "dbspa";
     private final String URL = "jdbc:mysql://localhost:3306/";
+    //Usuario que va a crear el pool de conexiones.
     private final String USER = "nuevoUsuario";
     private final String PASS = "123";
 
+    //Objeto statico de la clase para que solo exista una instancia.
     private static conexionDB dataSource;
+    //Objeto de la libreria commons-dbcp2
     private BasicDataSource basicDataSource;
 
+    private static final Logger LOGGER = Logger.getLogger(conexionDB.class.getName());
+
     private conexionDB(){
+        //Al momento de crear por primera vez la instancia se abre el pool de conexiones
         basicDataSource = new BasicDataSource();
+        //Se definen las propiedades de la base de datos como el driver, url, el usuario y la contraseña
         basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         basicDataSource.setUsername(USER);
         basicDataSource.setPassword(PASS);
@@ -29,24 +39,30 @@ public class conexionDB {
         basicDataSource.setMaxWait(Duration.ofMillis(10000));
     }
 
+    //Metodo para obtener la instancia de la clase con el patrón singleton
     public static conexionDB getIntance(){
+        //Si no existe la instancia la crea
         if(dataSource == null){
             dataSource = new conexionDB(); 
         }
+        //Devuelve la instancia
         return dataSource;
     }
 
-    public Connection getConnection(){
+
+    //El metodo get connection se encargará de otorgar una conexión de la pool a la base de datos.
+    public Connection getConnection() throws SQLException{
         Connection connect = null;
         try {
             connect = this.basicDataSource.getConnection();
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            LOGGER.log(Level.SEVERE,"Error al obtener la conexión a la base de datos", e);
+            throw e;
         }
         return connect;
-
     }
 
+    //Metodo para cerrar la conexión a la base de datos
     public void closeConnection (Connection connection) throws SQLException{
         connection.close();
     }
