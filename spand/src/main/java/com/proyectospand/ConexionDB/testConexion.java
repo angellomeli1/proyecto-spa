f807a5c45a65cfe839daa20cd8f497897f4de4ed
Angel Lomeli <2022030087@upsin.edu.mx>
2024-08-10 13:16:59 -0700

    Se hicieron unos cambios solo para poder probar el pool de conexiones con un usuario de la base de datos: actualmente ya se encuentra funcionando

--- spand/src/main/java/com/proyectospand/ConexionDB/testConexion.java
+++ spand/src/main/java/com/proyectospand/ConexionDB/testConexion.java
@@ -5,16 +5,24 @@ import java.sql.SQLException;
 
 public class testConexion {
     public static void main(String[] args) {
-        try {
-            Connection c = conexionDB.getIntance().getConnection();
-            if (c != null){
-                System.out.println("Conectado");
-                conexionDB.getIntance().closeConnection(c);
-            } else {
-                System.out.println("No conectado");
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+
+        validarCredenciales autenticarCredenciales = new validarCredenciales();
+
+        if(autenticarCredenciales.autenticarLogin("Irving Santos","Irvingon")){
+            System.out.println("El usuario ha ingresado al sistema.");
+            
+            try (Connection conexion = conexionDB.getIntance().getConnection()) {
+                if(conexion != null){
+                    System.out.println("El usuario se ha conectado a la base de datos.");
+                } else {
+                    System.out.println("Hubo un error al obtener una conexion");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } 
+
+        } else {
+            System.out.println("Usuario o contraseña incorrecta.");
         }
     }
 }
